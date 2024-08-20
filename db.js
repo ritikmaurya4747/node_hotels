9365a9e01dce172444d171bc0610b0ac34d6b699
Ritik Maurya <140969100+ritikmaurya4747@users.noreply.github.com>
2024-08-20 22:02:23 +0530

    vvvv

--- db.js
+++ db.js
@@ -9,7 +9,10 @@ const mongoURL = process.env.MONGODB_URL;
 
 //pass parameter inside curly braces  //Set up MongoDB connection 
 mongoose.connect(mongoURL)
-
+// {
+//     // useNewUrlParser: true,     //this is saying mongourl is new connection
+//     // useUnifiedTopology: true   //this line is mandatory
+// })
 
 //Get the default connection
 //Mongoose maintains a default connection representing the MongoDB connection
